b61c1889b81cdcbd7db7d9f2f6efb96b832c104a
Isaac Muscat <isaac.wieting.muscat@gmail.com>
2025-01-17 20:30:11 -0500

    Basic Bamboo Manager

---
+++ Assets/Scripts/BambooSeed.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BambooSeed : MonoBehaviour
+{
+    private List<GameObject> spawnedBamboo = new List<GameObject>(); // List to store spawned prefabs
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Grow() { 
+
+    }
+}
